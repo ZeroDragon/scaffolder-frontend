acaf23f47ebeba533c1f45adf6b32564cbef5aef
ZeroDragon <zr.drgn@gmail.com>
2017-10-23 23:28:10 -0500

    missed file from commit

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,11 +1,11 @@
-import _ from 'lodash';
-import $ from 'jquery';
+import _ from 'lodash'
+import $ from 'jquery'
 
 const component = () => {
-  const element = $('<div/>');
-  element.append(_.join(['hello', 'webpack'], ' '));
-  return element;
-};
+  const element = $('<div/>')
+  element.append(_.join(['hello', 'webpack'], ' '))
+  return element
+}
 $(document).ready(() => {
-  $('body').append(component());
-});
+  $('body').append(component())
+})
